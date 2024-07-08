b240d8ba27784ec5169240785e5e49f31d36263c
exert777 <alter0910@mail.ru>
2024-07-08 12:51:21 +0300

    добавит константы для удобства управления генерации моков

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 import { getRandomUnicInt, getRandomInt } from './utils';
 import { NAME, MESSAGE, DESCRIPTION_PHOTOS } from './mocks';
+import { QUNTITY_POST, QUNTITY_COMMETS, QUNTITY_AVATAR_COMMETS, MAX_ID_COMMENTS, MIN_QUNTITY_LIKES, MAX_QUNTITY_LIKES} from './const';
 
 
-const uniceIdPhoto = getRandomUnicInt(1, 25);
-const unicUrlPhoto = getRandomUnicInt(1, 25);
-const unicIdCometn = getRandomUnicInt(1, 250);
+const uniceIdPhoto = getRandomUnicInt(1, QUNTITY_POST);
+const unicUrlPhoto = getRandomUnicInt(1, QUNTITY_POST);
+const unicIdCometn = getRandomUnicInt(1, MAX_ID_COMMENTS);
 
 /* Возвращает случайное сообщение коментария */
 function getCommentMessage() {
   /* Переключатель определяет будет ли в сообщении одно или два предложения */
   const toggle = getRandomInt(1, 2);
 
-  return toggle === 2 ? `${MESSAGE[getRandomUnicInt(1, MESSAGE.length)()]}
-  + ${MESSAGE[getRandomUnicInt(1, MESSAGE.length)()]}` : `${MESSAGE[getRandomUnicInt(1, MESSAGE.length)()]}`;
+  return toggle === 2 ? `${MESSAGE[getRandomUnicInt(0, MESSAGE.length - 1)()]} ${MESSAGE[getRandomUnicInt(0, MESSAGE.length - 1)()]}` : `${MESSAGE[getRandomUnicInt(0, MESSAGE.length - 1)()]}`;
 }
 
 /* Программа генерирует один коментарий */
 function getUserComment() {
   return {
     id: unicIdCometn(),
-    avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+    avatar: `img/avatar-${getRandomInt(1, QUNTITY_AVATAR_COMMETS)}.svg`,
     message: getCommentMessage(),
     name: NAME[getRandomInt(0, NAME.length - 1)],
   };
@@ -33,14 +33,16 @@ function getUserPost() {
     id: uniceIdPhoto(),
     url: `photos/${unicUrlPhoto()}.jpg`,
     description: DESCRIPTION_PHOTOS[getRandomInt(0, DESCRIPTION_PHOTOS.length - 1)],
-    likes: getRandomInt(15, 200),
-    comments: getUserComment()
+    likes: getRandomInt(MIN_QUNTITY_LIKES, MAX_QUNTITY_LIKES),
+    comments: Array.from({length: getRandomInt(1, QUNTITY_COMMETS)}, getUserComment)
   };
 
 }
 
 function getUserPostData(){
-  return Array.from({length: 25}, getUserPost);
+  return Array.from({length: QUNTITY_POST}, getUserPost);
 }
 
 const userPostData = getUserPostData();
+
+console.log(userPostData)
